f8edac092066f94ccdb56aef8af0dcc493212aac
Gabriel Rosniak <rosniakid@gmail.com>
2026-08-04 22:13:57 -0300

    inicializando o projeto no github

---
+++ src/main/java/rastreamento/tempo_real/TempoRealApplication.java
@@ -0,0 +1,13 @@
+package rastreamento.tempo_real;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempoRealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempoRealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rastreamento/tempo_real/TempoRealApplicationTests.java
@@ -0,0 +1,13 @@
+package rastreamento.tempo_real;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempoRealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

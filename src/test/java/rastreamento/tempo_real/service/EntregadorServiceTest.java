001f3909355c210f055459445a60ab63217076f8
Gabriel Rosniak <rosniakid@gmail.com>
2026-08-20 23:31:18 -0300

    test: adicionando mais testes ao projeto

--- src/test/java/rastreamento/tempo_real/service/EntregadorServiceTest.java
+++ src/test/java/rastreamento/tempo_real/service/EntregadorServiceTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import rastreamento.tempo_real.dto.entregador.EntregadorRequestDto;
+import rastreamento.tempo_real.dto.entregador.EntregadorResponseDto;
+import rastreamento.tempo_real.dto.entregador.EntregadorUpdateDto;
 import rastreamento.tempo_real.enums.StatusEntrega;
 import rastreamento.tempo_real.enums.StatusEntregador;
 import rastreamento.tempo_real.exception.BusinessException;
@@ -16,6 +19,7 @@ import rastreamento.tempo_real.repository.EntregadorRepository;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -77,5 +81,31 @@ public class EntregadorServiceTest {
                 service.deletar(1L));
 
         verify(repository, never()).deleteById(any());
+    }
+
+    @Test
+    public void deveAtualizarCorretamente() {
+        EntregadorEntity entregador = EntregadorEntity
+                .builder()
+                .id(1L)
+                .statusEntregador(StatusEntregador.DISPONIVEL)
+                .build();
+
+        EntregadorUpdateDto dto = new EntregadorUpdateDto(
+                "João Atualizado",
+                "47988888888",
+                StatusEntregador.OFFLINE
+        );
+
+        when(repository.findById(1L)).thenReturn(Optional.of(entregador));
+        when(repository.save(any(EntregadorEntity.class))).thenReturn(entregador);
+
+        service.atualizar(1L, dto);
+
+        assertEquals("João Atualizado", entregador.getNome());
+        assertEquals("47988888888", entregador.getTelefone());
+        assertEquals(StatusEntregador.OFFLINE, entregador.getStatusEntregador());
+
+        verify(repository).save(entregador);
     }
 }
